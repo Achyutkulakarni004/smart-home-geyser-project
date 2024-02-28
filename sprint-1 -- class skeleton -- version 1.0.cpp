cd4518c90bf74fc9198e51f8397d18b6e9167f5f
Achyutkulakarni004 <161581921+Achyutkulakarni004@users.noreply.github.com>
2024-02-28 15:58:13 +0530

    Create sprint-1 -- class skeleton -- version 1.0.cpp

---
+++ sprint-1 -- class skeleton -- version 1.0.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+class EnvironmentSensor {
+private:
+    int currentTemperature;
+public:
+    int getCurrentTemperature() const {
+        return currentTemperature;
+    }
+
+    void setCurrentTemperature(int temperature) {
+        currentTemperature = temperature;
+    }
+};
+
+class WaterHeater {
+private:
+    int heatingTemperature;
+public:
+    int getHeatingTemperature() const {
+        return heatingTemperature;
+    }
+
+    void setHeatingTemperature(int temperature) {
+        heatingTemperature = temperature;
+    }
+};
+
+class SmartGeyser {
+protected:
+    EnvironmentSensor temperatureSensor;
+    WaterHeater waterHeater;
+public:
+    virtual void activateGeyser() = 0; // Virtual function to be overridden in derived classes
+};
+
+class HeatingSystem {
+public:
+    int calculateRequiredTemperature(int currentTemperature) {
+        return currentTemperature + 10; // Example calculation for required temperature
+    }
+};
+
+class BathroomGeyser : public SmartGeyser {
+private:
+    HeatingSystem heatingSystem;
+public:
+    void activateGeyser() override {
+        int currentTemperature = temperatureSensor.getCurrentTemperature();
+        int requiredTemperature = heatingSystem.calculateRequiredTemperature(currentTemperature);
+        waterHeater.setHeatingTemperature(requiredTemperature);
+        std::cout << "Heating temperature set to: " << requiredTemperature << " degrees" << std::endl;
+    }
+};
+
+class HomeOwner {
+public:
+    void getsOutOfBed(SmartGeyser& geyser) {
+        geyser.activateGeyser();
+    }
+};
+
+int main() {
+    HomeOwner owner;
+    BathroomGeyser geyser;
+    owner.getsOutOfBed(geyser);
+
+    return 0;
+}
